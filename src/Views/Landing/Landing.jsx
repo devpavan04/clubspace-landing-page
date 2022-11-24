784835e8469bab88b594d8d226679e29a451d296
Pavan Soratur <pavanvsoratur@gmail.com>
2022-11-23 21:03:29 -0800

    cleanup

---
+++ src/Views/Landing/Landing.jsx
@@ -0,0 +1,12 @@
+import banner from '../../../src/assets/banner.png';
+import './landing.css';
+
+const Landing = () => {
+  return (
+    <div>
+      <img src={banner} alt='' />
+    </div>
+  );
+};
+
+export default Landing;
